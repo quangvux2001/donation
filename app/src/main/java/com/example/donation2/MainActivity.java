c0560b24237dcad3df5dccb1691af533d780c3aa
unknown <quangvux2001>
2021-12-23 23:26:03 +0700

    abc

---
+++ app/src/main/java/com/example/donation2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.donation2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import android.support.*;
+//import android.view.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
